3db1373185094ad7105ab9300e7485e667e70e22
S Govind <117043723+Shyami4@users.noreply.github.com>
2025-11-08 20:18:42 -0800

    Final cleanup

--- tests/test_analysis_engine.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from analysis_engine import analyze_data
-
-def test_analyze_data_basic():
-    df = pd.DataFrame({"a":[1,2,3,100], "b":[2,3,4,5]})
-    out = analyze_data(df)
-    assert "summary" in out and "anomalies" in out
